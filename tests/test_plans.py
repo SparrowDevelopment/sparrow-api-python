04b9f9c424d71d54d12fca49af8a97a8d5f8a12e
Alexander Pushkov <hi@ale.rocks>
2017-11-22 23:32:40 +0300

    Update test_plans with suggesions by @ricklove

--- tests/test_plans.py
+++ tests/test_plans.py
@@ -5,7 +5,6 @@ import sparrowone
 
 
 M_KEY = "CUK5YODHVAZHFBM6KESZO1J4"
-ACH_M_KEY = "RZOZ2AMMYF7GX2VF1L05WW1G"
 
 class PlansCreateTestCase(unittest.TestCase):
     def setUp(self):
@@ -17,31 +16,11 @@ class PlansCreateTestCase(unittest.TestCase):
             planname="PaymentPlan1",
             plandesc="1st Payment Plan",
             startdate="01/31/2017",
-            defaultachmkey=ACH_M_KEY,
-            defaultcreditcardmkey=M_KEY,
-            defaultcheckmkey=M_KEY,
-            defaultstarcardmkey=M_KEY,
-            defaulte_walletmkey=M_KEY,
             sequence_1=1,
             amount_1=1.99,
             scheduletype_1="custom",
             scheduleday_1=7,
             duration_1=365,
-            productid_1="abc",
-            description_1="Weekly",
-            sequence_2=2,
-            amount_2=2.99,
-            scheduletype_2="monthly",
-            scheduleday_2=1,
-            duration_2=-1,
-            productid_2="123",
-            description_2="Monthly",
-            notifyfailures=False,
-            userecycling=True,
-            retrycount=2,
-            retrytype="daily",
-            retryperiod=1,
-            autocreateclientaccounts=True,
         ))
         self.assertEqual(resp["textresponse"], "SUCCESS")
 
@@ -54,38 +33,16 @@ class PlansTestCase(unittest.TestCase):
             planname="PaymentPlan1",
             plandesc="1st Payment Plan",
             startdate="01/31/2017",
-            defaultachmkey=ACH_M_KEY,
-            defaultcreditcardmkey=M_KEY,
-            defaultcheckmkey=M_KEY,
-            defaultstarcardmkey=M_KEY,
-            defaulte_walletmkey=M_KEY,
             sequence_1=1,
             amount_1=1.99,
             scheduletype_1="custom",
             scheduleday_1=7,
             duration_1=365,
-            productid_1="abc",
-            description_1="Weekly",
-            sequence_2=2,
-            amount_2=2.99,
-            scheduletype_2="monthly",
-            scheduleday_2=1,
-            duration_2=-1,
-            productid_2="123",
-            description_2="Monthly",
-            notifyfailures=False,
-            userecycling=True,
-            retrycount=2,
-            retrytype="daily",
-            retryperiod=1,
-            autocreateclientaccounts=True,
         ))
         self.plan_token = resp["plantoken"]
         resp = self.sprw.customers.create(data=dict(
             firstname="John",
             lastname="Doe",
-            username="py_test_user_%r" % uuid.uuid1(),
-            email="john@example.com",
             paytype_1="creditcard",
             cardnum_1="4111111111111111",
             cardexp_1="1019"
